8bef62df2742be7811574753b0863d22e67ac250
Miguel Negron Garcia <josuefrontend@gmail.com>
2023-11-17 13:34:47 -0600

    I change the text argument as key or identifier to the id made with the library uuid to the component AppUI.

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -33,11 +33,11 @@ function AppUI() {
                                 {!loading && searchedTodos.length === 0 && <EmptyTodos/>}
                                 {searchedTodos.map(todo => (
                                 <TodoItem  
-                                key={todo.text}  
+                                key={todo.id}  
                                 text={todo.text}
                                 completed={todo.completed}
-                                onComplete={() => completeTodo(todo.text)}
-                                onDelete={()=> deleteTodo(todo.text)}
+                                onComplete={() => completeTodo(todo.id)}
+                                onDelete={()=> deleteTodo(todo.id)}
                                 />  
                                 ))}
                             </TodoList>
